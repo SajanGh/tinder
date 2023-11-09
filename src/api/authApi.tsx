f3006d58801b0808fa40ccfc0625db63bc291adb
Saugat Ghimire <saugat77ghimire@gmail.com>
2023-11-09 08:40:13 +0545

    valiation and some changes

---
+++ src/api/authApi.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseURL = "http://127.0.0.1:3000/v1";
+
+export const authApi = axios.create({
+  baseURL,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+authApi.defaults.headers.common["Content-Type"] = "application/json";
